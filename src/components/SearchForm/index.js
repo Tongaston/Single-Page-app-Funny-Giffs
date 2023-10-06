3fc335c34e3cddbb68fc6f1fc7c6bfabb8632070
Tongaston <gaston.moure@gmail.com>
2023-10-06 18:08:35 +0200

    useReducer y nuevo hook

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,23 +1,51 @@
-import React, {useState} from "react";
+import React from "react";
+import useForm from "./hook";
+import { useLocation } from "wouter";
 
 
-function SearchForm({ onSubmit }) {
-    const [keyword, setKeyword] = useState("");
+const RATINGS = ["g", "pg", "pg-13", "r"];
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        onSubmit({ keyword })
-    }
+function SearchForm({
+    initialKeyword = '',
+    initialRating = 'g',
+}) {
+    const { keyword, rating, times, updateKeyword, updateRating } = useForm({
+        initialKeyword,
+        initialRating,
+    });
+
+    const [_, pushLocation] = useLocation()
 
     const handleChange = (evt) => {
-        setKeyword(evt.target.value)
+        updateKeyword(evt.target.value)
+    };
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        // navegar a otra ruta
+        pushLocation(`/search/${keyword}/${rating}`)
+    }
+
+    const handleChangeRating = (evt) => {
+        updateRating(evt.target.value)
     }
 
     return (
         <form onSubmit={handleSubmit} className="form-container">
-            <input className='form-input' placeholder="search your favourite Gif!" 
-            onChange={handleChange} type="text" value={keyword} />
+            <input className='form-input'
+                placeholder="search your favourite Gif!"
+                onChange={handleChange}
+                type="text"
+                value={keyword}
+            />
             <button className="form-btn" >Buscar</button>
+            <select className="form-select" value={rating} onChange={handleChangeRating}>
+                <option disabled>Rating type</option>
+                {RATINGS.map((rating) => (
+                    <option key={rating}>{rating}</option>
+                ))}
+            </select>
+            <small>{times} veces</small>
         </form>
     )
 }
--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -5,7 +5,7 @@ import GifsContext from '../context/GifsContext'
 
 const INITIAL_PAGE = 0
 
-export function useGifs({ keyword } = { keyword: 'random' }) {
+export function useGifs({ keyword, rating } = { keyword: null }) {
   const [loading, setLoading] = useState(false)
   const[loadingNextPage, setLoadingNextPage] = useState(false)
   const [page, setPage] = useState(INITIAL_PAGE)
@@ -19,26 +19,26 @@ export function useGifs({ keyword } = { keyword: 'random' }) {
   useEffect(function () {
     setLoading(true)
 
-    getGifs({ keyword: keywordToUse, page })
+    getGifs({ keyword: keywordToUse, rating })
       .then(gifs => {
         setGifs(gifs)
         setLoading(false)
         //guardamos la keyword en el localStorage
         localStorage.setItem('lastKeyword', keyword)
       })
-  }, [keyword,keywordToUse, setGifs])
+  }, [keyword, keywordToUse, rating, setGifs])
 
   useEffect(function () {
 if (page === INITIAL_PAGE) return
 
 setLoadingNextPage(true)
 
-getGifs({ keyword: keywordToUse, page })
+getGifs({ keyword: keywordToUse, rating, page })
 .then(nextGifs => {
 setGifs(prevGifs => prevGifs.concat(nextGifs))
 setLoadingNextPage(false)
 })
-  }, [keywordToUse, page, setGifs])
+  }, [keywordToUse, page, rating, setGifs])
 
   return {loading, loadingNextPage, gifs, setPage }
 }
