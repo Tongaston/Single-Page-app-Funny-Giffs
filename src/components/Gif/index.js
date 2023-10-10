74d6d3a7e01774bb7c76d51ce18211337f1f73d2
Tongaston <gaston.moure@gmail.com>
2023-10-10 15:53:53 +0200

    Login y favoritos

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'wouter'
 import './Gif.css'
+import Fav from '../Fav'
 
 function Gif({title, id, url}) {
     return (
         <div className='Gif'>
+            <div className='Gif-buttons'>
+            <Fav id={id} />
+            </div>
         <Link to={`/gif/${id}`} className='Gif-link'>
             <h4>{title}</h4>
              <img loading='lazy' src={url} alt={title} />
